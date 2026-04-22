b79c53c1c551268bd3d1bd9ff11891e89879e749
oorucelik <o.oruccelik@gmail.com>
2026-04-22 12:13:21 +0300

    feature(source): Added stg_content_catalog.sql and _sources.yml files

--- models/staging/content/stg_content_catalog.sql
+++ models/staging/content/stg_content_catalog.sql
@@ -7,25 +7,25 @@
 with content as (
     select *,
         row_number() over (partition by content_id order by content_id desc) as rn
-    from {{ref('raw_content_catalog')}} 
+    from {{source('tmdb_api__content_catalog', 'raw_content_catalog')}}
 ),
 
 renamed as (
     select
         {{ dbt_utils.generate_surrogate_key(['content_id']) }} as content_key,
         content_id,
-        cast(tmdb_id as integer)            as tmdb_id,
+        cast(tmdb_id as integer) as tmdb_id,
         title,
         content_type,
-        genres                               as genre_list,       -- pipe-delimited, keep for exploding later
+        genres as genre_list,       -- pipe-delimited, keep for exploding later
         primary_genre,
-        cast(release_date as date)           as release_date,
-        cast(vote_average as decimal(3,1))   as vote_average,
-        cast(vote_count as integer)          as vote_count,
-        cast(popularity as decimal(10,2))    as popularity,
+        cast(release_date as date) as release_date,
+        cast(vote_average as decimal(3,1)) as vote_average,
+        cast(vote_count as integer) as vote_count,
+        cast(popularity as decimal(10,2)) as popularity,
         original_language,
         overview,
-        cast(runtime_minutes as integer)     as runtime_minutes
+        cast(runtime_minutes as integer) as runtime_minutes
     from content
     where rn = 1
 )
